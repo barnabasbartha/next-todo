cff2fc58c295f73e745d0add1479b1cb680ee7f6
Barnabás BARTHA <bartha.brw@gmail.com>
2025-05-09 09:50:10 +0200

    refactor: cleanup useTodos functions

--- apps/next-todo/src/hooks/useTodos.ts
+++ apps/next-todo/src/hooks/useTodos.ts
@@ -6,31 +6,19 @@ export const useTodos = () => {
   const { todos, setTodos, editingId, setEditingId } =
     useContext(TodoListContext);
 
-  const todoIndexById = useMemo(
-    () =>
-      todos.reduce(
-        (result, item, index) => ({ ...result, [item.id]: index }),
-        {} as { [key: string]: number }
-      ),
-    [todos]
-  );
-
   const openTodos = useMemo(() => todos.filter(({ open }) => open), [todos]);
   const doneTodos = useMemo(() => todos.filter(({ open }) => !open), [todos]);
 
   const toggleOpen = (id: string) => {
-    const index = todoIndexById[id];
-    const value = todos[index];
-    if (!value) return;
-    const newValue: Todo = {
-      ...value,
-      open: !value.open,
-    };
-    setTodos((values) => [
-      ...values.slice(0, index),
-      newValue,
-      ...values.slice(index + 1),
-    ]);
+    setTodos((items) =>
+      items.map((item) => {
+        if (item.id !== id) return item;
+        return {
+          ...item,
+          open: !item.open,
+        } satisfies Todo;
+      })
+    );
   };
 
   const setOrder = (id: string, destinationIndex: number) => {
@@ -49,29 +37,19 @@ export const useTodos = () => {
   };
 
   const editText = (id: string, text: string) => {
-    const index = todoIndexById[id];
-    const value = todos[index];
-    if (!value) return;
-    const newValue: Todo = {
-      ...value,
-      text,
-    };
-    setTodos((values) => [
-      ...values.slice(0, index),
-      newValue,
-      ...values.slice(index + 1),
-    ]);
+    setTodos((items) =>
+      items.map((item) => {
+        if (item.id !== id) return item;
+        return {
+          ...item,
+          text,
+        } satisfies Todo;
+      })
+    );
   };
 
   const deleteTodo = (id: string) => {
-    if (!id) return;
-    const index = todoIndexById[id];
-    const value = todos[index];
-    if (!value) return;
-    setTodos((values) => [
-      ...values.slice(0, index),
-      ...values.slice(index + 1),
-    ]);
+    setTodos((items) => items.filter((item) => item.id !== id));
   };
 
   const stopEditing = () => {
